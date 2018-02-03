b9414322f02a7fc4cc55911cdc8121e7fc8b0b4e
Alexander Lloyd <a.lloyd.2@bham.ac.uk>
2018-02-03 15:28:42 +0000

    Added more boilerplate

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from flask import Flask, Response, request
@@ -16,6 +17,43 @@ def index():
         return request.args["hub.challenge"], 200
 
     return "Hello world", 200
+
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
+    try:
+        if type(msg) is dict:
+            msg = json.dumps(msg)
+        else:
+            msg = unicode(msg).format(*args, **kwargs)
+        print u"{}: {}".format(datetime.now(), msg)
+    except UnicodeEncodeError:
+        pass  # squash logging errors in case of non-ascii text
+    sys.stdout.flush()
+
+
 
 
 @app.route('/callback')
