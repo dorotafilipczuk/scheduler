097bae5ec4f0989cee6d6e01988d98d403bf4dff
bmanga <bruno.manga95@gmail.com>
2018-02-03 20:20:42 +0000

    test

--- app.py
+++ app.py
@@ -144,7 +144,6 @@ def webhook():
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
-
     if data["object"] == "page":
 
         for entry in data["entry"]:
