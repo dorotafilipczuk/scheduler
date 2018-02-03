cbf9d15584491f5554a09624698d8a3c10893112
Alexander Lloyd <a.lloyd.2@bham.ac.uk>
2018-02-03 19:14:31 +0000

    Temp to send data back

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_message(sender_id, "roger that!")
+                    send_message(sender_id, json.dumps(data))
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
