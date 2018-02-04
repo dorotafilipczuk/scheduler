cf1b056364e43c5f31293906780970f817db3e8a
bmanga <bruno.manga95@gmail.com>
2018-02-04 06:08:06 +0000

    Check poll status

--- testfbchat.py
+++ testfbchat.py
@@ -28,36 +28,48 @@ class ScheduleBot(Client):
     WELCOME = "I am here to assist you. I'll help you schedule a meeting!"
     USER_NOT_LOGGED_IN = "@{name} has not logged in. Goto https://hackathon-scheduler.herokuapp.com/authorize/google/ and sign in. @Chronomatch Bot me when this has been completed"
 
+    def onPollUpdated(self, options):
+        print("please", maybe_finalize_meeting(options, filter(lambda u: u.uid != self.uid, self.fetchAllUsers())))
+
+
     def onMessage(self, mid=None, author_id=None, message=None, message_object=None, thread_id=None,
                   thread_type=ThreadType.USER, ts=None, metadata=None, msg=None):
+        print("I have problems", message_object.text or "shit")
         self.markAsDelivered(author_id, thread_id)
         self.markAsRead(author_id)
 
-        if '@Chronomatch Bot' in message_object.text and author_id != self.uid:
-            self.setTypingStatus(TypingStatus.TYPING, thread_id, thread_type)
 
-            self.send(Message(ScheduleBot.WELCOME), thread_id, thread_type)
+        if '@Chronomatch Bot' in message_object.text and author_id != self.uid:
+            #self.setTypingStatus(TypingStatus.TYPING, thread_id, thread_type)
+
+            #self.send(Message(ScheduleBot.WELCOME), thread_id, thread_type)
             # check all user are in firebase
             # remove bot
             us = filter(lambda u: u.uid != self.uid, self.fetchAllUsers())
 
-            notloggedin = users_logged_in(us)
-            print(notloggedin)
-
-            if len(notloggedin) != 0:
-                for user in notloggedin:
-                    m = Message(ScheduleBot.USER_NOT_LOGGED_IN.format(name=(user.name)))
-
-                    self.send(m, thread_id=thread_id, thread_type=thread_type)
-                    time.sleep(0.2)
-                return
-
+            print("us is", us)
+            #notloggedin =  (us)
+            print("here")
+            #print(notloggedin)
             createPole(["It", "Works"])
-        self.setTypingStatus(TypingStatus.STOPPED, thread_id, thread_type)
+        #self.setTypingStatus(TypingStatus.STOPPED, thread_id, thread_type)
+
+
+def maybe_finalize_meeting(poll_opts, all_users):
+    best_option = poll_opts[0]
+    users_voted = []
+    for option in poll_opts:
+        if option["total_count"] > best_option["total_count"]:
+            best_option = option
+        users_voted = list(set().union(users_voted, option["voters"]))
+    print ("best match is ", best_option)
+    print("users voted", users_voted)
+    return set(all_users) == set(users_voted)
 
 
 def users_logged_in(users):
     userList = []
+    f = firebase.FirebaseApplication('https://schedule-03022018.firebaseio.com/', None)
     for user in users:
         uid = user.uid
         u = f.get('/user', uid)
